ea3afa46109b1b33f54654d5c73fdfc9b01e276e
Vikas Gola <vikasgola2015@gmail.com>
2020-03-05 23:00:36 +0530

    buttons

--- index.js
+++ index.js
@@ -1,46 +1,52 @@
 let viewer1 = OpenSeadragon({
     id: "viewer1",
-    prefixURL: "openseadrag/images/",
-    tileSources:{
+    prefixURL: "/coordinated-view-code-challenge/",
+    tileSources: {
         url: "http://www.webpathology.com/slides-13/slides/Thyroid_MTC42_PseudoPapillary.jpg",
         type: 'image',
     },
+    zoomInButton: "zoom-in1",
+    zoomOutButton: "zoom-out1",
+    homeButton: "home1",
 });
 
 let viewer2 = OpenSeadragon({
     id: "viewer2",
-    prefixURL: "openseadrag/images/",
-    tileSources:{
+    prefixURL: "/coordinated-view-code-challenge/",
+    tileSources: {
         url: "http://www.webpathology.com/slides-13/slides/Thyroid_MTC42_PseudoPapillary.jpg",
         type: 'image',
     },
+    zoomInButton: "zoom-in2",
+    zoomOutButton: "zoom-out2",
+    homeButton: "home2",
 });
 
 
-$("#synccheckbox").change(function() {
-    if(this.checked) {
-        ["zoom", "pan"].forEach((eventname)=>{
+$("#synccheckbox").change(function () {
+    if (this.checked) {
+        ["zoom", "pan"].forEach((eventname) => {
             let isZoomHappened = false;
             let isPanHappened = false;
-            [viewer1, viewer2].forEach((viewer)=>{
-                viewer.addHandler(eventname, (v)=>{
-                    let other = viewer == viewer1 ? viewer2: viewer1;
-        
-                    if(!isZoomHappened){
+            [viewer1, viewer2].forEach((viewer) => {
+                viewer.addHandler(eventname, (v) => {
+                    let other = viewer == viewer1 ? viewer2 : viewer1;
+
+                    if (!isZoomHappened) {
                         isZoomHappened = !isZoomHappened;
                         other.viewport.zoomTo(viewer.viewport.getZoom());
                         isZoomHappened = false;
                     }
-                    if(!isPanHappened){
+                    if (!isPanHappened) {
                         isPanHappened = !isPanHappened;
                         other.viewport.panTo(viewer.viewport.getCenter());
                         isPanHappened = false;
                     }
                 });
             })
-        });        
-    }else{
-        [viewer1, viewer2].forEach((viewer)=>{
+        });
+    } else {
+        [viewer1, viewer2].forEach((viewer) => {
             viewer.removeAllHandlers();
         });
     }
